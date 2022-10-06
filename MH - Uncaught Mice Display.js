d5cd91c4ed9627930998f105c9588be5a7cc7cac
Chromatically <85063206+Chromatically@users.noreply.github.com>
2022-10-06 15:10:25 +0800

    Update MH - Uncaught Mice Display.js

--- MH - Uncaught Mice Display.js
+++ MH - Uncaught Mice Display.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MH - Uncaught Mice Dispaly
-// @version      1.0
+// @version      1.0.1
 // @description  Shows uncaught mice at any location
 // @author       MI
 // @match        https://www.mousehuntgame.com/*
@@ -10,8 +10,28 @@
 // @namespace https://greasyfork.org/users/748165
 // ==/UserScript==
 
-(function() {
-    if (user.environment_name)
-      render()
-})();
+$(document).ready(function(){
+  if($(".campPage-trap-armedItemContainer")[0]){
+     typeof $("#mi-uncaught-box")[0] == "object"? null : uncaughtBoxRender()
+  }
+})
 
+//Renders the box
+function uncaughtBoxRender(){
+  var locationWrapper = $("mousehuntHud-environmentIconWrapper")[0]
+
+  //Box styles
+  var uncaughtBtn = document.createElement("button");
+  uncaughtBtn.style.backgroundSize = "30px"
+  uncaughtBtn.className = "mi-uncaught-box";
+  uncaughtBtn.style.position = absolute;
+  uncaughtBtn.style.width = "30px";
+  uncaughtBtn.style.height = "30px";
+  uncaughtBtn.style.borderRadius = "5px";
+
+  locationWrapper.insertAdjacentElement("afterend",uncaughtBtn)
+
+
+
+
+}
