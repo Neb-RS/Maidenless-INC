9eccf76b8fe4755cd6f000490db8dd5861cab96d
Chromatically <85063206+Chromatically@users.noreply.github.com>
2022-10-06 15:39:59 +0800

    Update MH - Uncaught Mice Display.js

--- MH - Uncaught Mice Display.js
+++ MH - Uncaught Mice Display.js
@@ -32,9 +32,43 @@ function uncaughtBoxRender(){
   uncaughtBtn.style.background = "#e5dac0";
   uncaughtBtn.style.borderColor = "#9f9171";
 
+  //Button function --- 
+  //Firstly calls for the locaiton informations
+  uncaughtBtn.onclick = function(){
+    console.log("Requesting Information of Location Mice from Server");
+    postReq("https://www.mousehuntgame.com/managers/ajax/pages/page.php"
+    `sn=Hitgrab&hg_is_ajax=1&page_class=HunterProfile&page_arguments%5Btab%5D=mice&page_arguments%5Bsub_tab%5D=location&last_read_journal_entry_id=${lastReadJournalEntryId}&uh=${user.unique_hash}`
+    ). then(res =>{
+      try{
+        var response = JSON.parse(res.responseText);
+        if (response){
+          var miceList = response.page.tabs.mice.subtabs[1].mouseList.categories;
+          console.log(miceList);
+        }
+      } catch (error){
+        console.log(error)
+      }
+    })
+  },
+  
+
   locationWrapper.insertAdjacentElement("afterend",uncaughtBtn)
 
+}
 
-
-
+function postReq(url, form) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        resolve(this);
+      }
+    };
+    xhr.onerror = function () {
+      reject(this);
+    };
+    xhr.send(form);
+  });
 }
