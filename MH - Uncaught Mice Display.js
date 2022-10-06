d43a8903d1bbf6d952f3290144cafdd9198a03d0
Chromatically <85063206+Chromatically@users.noreply.github.com>
2022-10-06 15:55:25 +0800

    Update MH - Uncaught Mice Display.js

--- MH - Uncaught Mice Display.js
+++ MH - Uncaught Mice Display.js
@@ -48,7 +48,7 @@ function uncaughtBoxRender(){
           //console.log(miceListCategory);
           //Loops through the parsed data to find the matching location
           for(var i=0; 0 < miceListCategory.length;i++){
-            if(miceListCategory[i].type = currentLocation){
+            if(miceListCategory[i].type == currentLocation){
               console.log("User is currently in " + miceListCategory[i]);
             }
           }
