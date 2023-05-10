4f94f84dff77efbb7f937a7f28cbd929aba1d369
Neb <75031454+Neb-RS@users.noreply.github.com>
2023-05-10 02:20:31 +0200

    Version up
    Would be nice if I actually remembered to change the version in time, wouldn't it.

--- MH_-_Minluck__CRE_tool_v2.0_new.user.js
+++ MH_-_Minluck__CRE_tool_v2.0_new.user.js
@@ -5,7 +5,7 @@
 // @match        https://www.mousehuntgame.com/*
 // @match        https://apps.facebook.com/mousehunt/*
 // @icon         https://www.google.com/s2/favicons?domain=mousehuntgame.com
-// @version      3.1.0
+// @version      3.2.0
 // @grant        none
 // @namespace https://greasyfork.org/users/748165
 // ==/UserScript==
