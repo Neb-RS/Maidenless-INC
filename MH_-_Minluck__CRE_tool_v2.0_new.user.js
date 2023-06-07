65e8d8a08cc784772defc869eb10bb4a30c3c08f
LeppyMH <86115973+LeppyMH@users.noreply.github.com>
2023-06-07 22:36:29 +1000

    Update MH_-_Minluck__CRE_tool_v2.0_new.user.js
    Add support for Absolute Acolyte, Warmonger and Artillery Commander being invincible.

--- MH_-_Minluck__CRE_tool_v2.0_new.user.js
+++ MH_-_Minluck__CRE_tool_v2.0_new.user.js
@@ -5,7 +5,7 @@
 // @match        https://www.mousehuntgame.com/*
 // @match        https://apps.facebook.com/mousehunt/*
 // @icon         https://www.google.com/s2/favicons?domain=mousehuntgame.com
-// @version      3.3.0
+// @version      3.3.1
 // @grant        none
 // @namespace https://greasyfork.org/users/748165
 // ==/UserScript==
@@ -5370,6 +5370,30 @@ function specialMPEff(mouseName, mouse_power, eff) {
             // Zurreal's Folly is the only effective weapon against Zurreal the Eternal.
             logCRAdjustmentInfo(mouseName, "Zurreal the Eternal");
             eff = 0;
+        }
+    }
+
+    // Special case: Absoulte Acolyte
+    if (locationName == "Bristle Woods Rift") {
+        if (mouseName == "Absoulte Acolyte") {
+            var acolyteSandContainer = document.getElementsByClassName("riftBristleWoodsHUD-acolyteStats-acolyteSand")[0];
+            if (acolyteSandContainer && acolyteSandContainer.textContent != "0") {
+                // Absoulte Acolyte is invincible while sand not fully drained.
+                logCRAdjustmentInfo(mouseName, "Absoulte Acolyte");
+                eff = 0;
+            }
+        }
+    }
+
+    // Special case: Warmonger and Artillery Commander
+    if (locationName == "Fiery Warpath") {
+        if (mouseName == "Warmonger" || mouseName == "Artillery Commander") {
+            var guardContainer = document.getElementsByClassName("warpathHUD-wave-mouse mousehuntTooltipParent desert_elite_gaurd")[0];
+            if (guardContainer && guardContainer.classList[3] == "active") {
+                // Warmonger and Artillery Commander are invincible while guarded.
+                logCRAdjustmentInfo(mouseName, "Warmonger");
+                eff = 0;
+            }
         }
     }
 
