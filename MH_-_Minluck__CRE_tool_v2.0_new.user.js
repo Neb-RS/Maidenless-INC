71a3c59e8da5f69ffc4f168163dc838974dfac1d
Neb <75031454+Neb-RS@users.noreply.github.com>
2024-02-14 23:51:06 +0100

    Fix auto-update
    Encoding doesn't like the thunder
    Dumpling Chef change still active

--- MH_-_Minluck__CRE_tool_v2.0_new.user.js
+++ MH_-_Minluck__CRE_tool_v2.0_new.user.js
@@ -1344,7 +1344,7 @@ var allMiceInfo = {
     },
     "Dumpling Chef": {
         "power": 2000,
-        "effs": [0, 0, 0, 0, 0, 100, 0, 175, 0, 0]
+        "effs": [100, 100, 100, 100, 100, 100, 100, 175, 100, 100]
     },
     "Dumpling Delivery": {
         "power": 2305,
@@ -4322,7 +4322,7 @@ var allMiceInfo = {
         "power": 16500,
         "effs": [0, 300, 0, 0, 0, 0, 0, 0, 0, 0]
     },
-    "\u26a1Thunderlord\u26a1": {
+    "⚡Thunderlord⚡": {
         "power": 13500,
         "effs": [0, 300, 0, 0, 0, 0, 0, 0, 0, 0]
     },
