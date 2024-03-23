b7713d5eb03a0bf5fb418a32b9be180765eceab9
Neb <75031454+Neb-RS@users.noreply.github.com>
2024-03-23 11:18:45 +0100

    Comma changes in ARs

--- MH_-_Minluck__CRE_tool_v2.0_new.user.js
+++ MH_-_Minluck__CRE_tool_v2.0_new.user.js
@@ -4813,7 +4813,7 @@ var allType = ['Arcane', 'Draconic', 'Forgotten', 'Hydro', 'Parental', 'Physical
 var dragonbaneCharmMice = new Set([
     "Dragon",
     "Dragoon",
-    "Ful'Mina, The Mountain Queen",
+    "Ful'Mina the Mountain Queen",
     "Thunder Strike",
     "Thundering Watcher",
     "⚡Thunderlord⚡",
@@ -4821,7 +4821,7 @@ var dragonbaneCharmMice = new Set([
     "Fuzzy Drake",
     "Cork Defender",
     "Burly Bruiser",
-    "Corky, the Collector",
+    "Corky the Collector",
     "Horned Cork Hoarder",
     "Rambunctious Rain Rumbler",
     "Corkataur",
@@ -4836,8 +4836,8 @@ var dragonbaneCharmMice = new Set([
     "Bearded Elder",
     "Ignatia",
     "Cinderstorm",
-    "Bruticus, the Blazing",
-    "Stormsurge, the Vile Tempest",
+    "Bruticus the Blazing",
+    "Stormsurge the Vile Tempest",
     "Kalor'ignis of the Geyser",
     "Tiny Dragonfly",
     "Lancer Guard",
@@ -6259,13 +6259,13 @@ function getArInfoForMoussuPicchu(stageInfo) {
                 case "max":
                     return {
                         "FTC": 0.00,
-                        "Ful'Mina, The Mountain Queen": 1.0000,
+                        "Ful'Mina the Mountain Queen": 1.0000,
                     };
                 case "high":
                     return {
                         "FTC": 0.00,
                         "Thundering Watcher": 0.5831,
-                        "Ful'Mina, The Mountain Queen": 0.3699,
+                        "Ful'Mina the Mountain Queen": 0.3699,
                         "Dragoon": 0.0470,
                     };
                 case "medium":
