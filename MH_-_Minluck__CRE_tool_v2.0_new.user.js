bba4e59475537c6fc5bcd9ec43b53468ccbb472c
Neb-RS <nebhetrs@gmail.com>
2024-08-15 03:17:46 +0200

    Version properly up

--- MH_-_Minluck__CRE_tool_v2.0_new.user.js
+++ MH_-_Minluck__CRE_tool_v2.0_new.user.js
@@ -5,7 +5,7 @@
 // @match        https://www.mousehuntgame.com/*
 // @match        https://apps.facebook.com/mousehunt/*
 // @icon         https://www.google.com/s2/favicons?domain=mousehuntgame.com
-// @version      4.11.0
+// @version      5.0.0
 // @grant        none
 // @namespace https://greasyfork.org/users/748165
 // ==/UserScript==
